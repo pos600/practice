6ec6348f935d4614cde95367e12eb044399af3da
nito <80657161+pos600@users.noreply.github.com>
2023-02-23 01:37:30 +0800

    Add files via upload

---
+++ First/src/App.java
@@ -0,0 +1,142 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // System.out.println("What is your name?");
+        // String name = scanner.nextLine();
+        // System.out.println("How old are you?");
+        // int age = scanner.nextInt();
+        // scanner.nextLine();
+        // System.out.println("What is your favorite food?");
+        // String food = scanner.nextLine();
+
+        // System.out.println("Hello " + name);
+        // System.out.println("You are " + age);
+        // System.out.println("And you like eating " + food);
+        
+        // String name = JOptionPane.showInputDialog("What is your name? ");
+        // JOptionPane.showMessageDialog(null, "Hello " + name);
+
+        // int age = Integer.parseInt(JOptionPane.showInputDialog("How old are you?"));
+        // JOptionPane.showMessageDialog(null, "You are " + age + " years of age.");
+
+        // Double gpa = Double.parseDouble(JOptionPane.showInputDialog("GPA: "));
+        // JOptionPane.showMessageDialog(null, "Your GPA is: " + gpa);
+
+        // double x ;
+        // double y ;
+        // double t ;
+
+        // System.out.println("First number: ");
+        // double firstNum = scanner.nextInt();
+        // System.out.println("Second number: ");
+        // double secondNum = scanner.nextInt();
+        // scanner.nextLine();
+        // System.out.println("Your name: ");
+        // String name = scanner.nextLine();
+
+        // double hypo = Math.sqrt((firstNum * firstNum)+(secondNum * secondNum));
+
+        // System.out.println(hypo + "and u are " + name);
+
+
+        // Random random = new Random();
+        // int x = random.nextInt(6) + 1;
+        // double y = random.nextDouble();
+        // boolean z = random.nextBoolean();
+        // System.out.println(z);
+
+        // int age = 14;
+
+        // if(age >= 75){
+        //     System.out.println("damn, you old");
+        // } else if(age >= 50){
+        //     System.out.println("you aight");
+        // }else if(age >= 30){
+        //     System.out.println("Youre at working age, well past that but still.");
+        // }else if(age >= 15){
+        //     System.out.println("play a lot of videogames youll lose the chance to when you grow up");
+        // }else {
+        //     System.out.println("damn you a fetus");
+        // }
+
+        // String day = "xd";
+
+        // switch(day){
+        //     case "Saturday":System.out.println("its saturday yo.");
+        //     break;
+        //     case "Sunday":System.out.println("class tomorrow fuck this");
+        //     break;
+        //     case "Monday":System.out.println("ahh, death.");
+        //     break;
+        //     case "Tuesday":System.out.println("hope is no more.");
+        //     break;
+        //     case "Wednesday":System.out.println("stop, dont give me hope.");
+        //     break;
+        //     case "Thursday":System.out.println("perhaps?");
+        //     break;
+        //     case "Friday":System.out.println("i cant fucking believe it, god has not forsaken me.");
+        //     break;
+        //     default: System.out.println("ahh youre dumb af yo");
+        // }
+
+        // int x = 19;
+        // if (x < 10 && x > 5){
+        //     System.out.println("x is less than 10 and is greater than 5.");
+        // } else if (x > 10 && x < 20){
+        //     System.out.println("x is greater than 10 but less than 20");
+        // }
+
+        // System.out.println("Quit game? ");
+        // String decision = scanner.nextLine();
+
+        // if (decision.equals("Q") || decision.equals("q")) {
+        //     System.exit(0);
+        // }else {
+        //     System.out.println("in progress. . . ");
+        // }
+
+        // String name = "";
+
+        // while(name.isBlank()){
+        //     System.out.println("Enter your name: ");
+        //     name = scanner.nextLine();
+        // }
+
+        // System.out.println("Welcome " + name);
+
+
+        // for (int i=1; i<11; i++){
+        //     System.out.println(i);
+        // }
+
+        // int rows;
+        // int cols; 
+        // String symbol ="";
+
+        // System.out.println("Enter # of rows: ");
+        // rows = scanner.nextInt();
+        // System.out.println("Enter # of columns: ");
+        // cols = scanner.nextInt();
+
+        // System.out.println("Input the symbol: ");
+        // symbol = scanner.next();
+
+        // for (int i = 1; i <= rows; i++){
+        //     System.out.println();
+        //     for (int j = 1; j <= cols; j++){
+        //         System.out.print(symbol);
+        //     }
+        // }
+
+        
+
+
+    }
+}
